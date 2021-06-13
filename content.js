98100e43d5a2273f63a634f6d85c820db69b156a
naman1608 <1608naman@gmail.com>
2021-06-13 18:11:16 +0530

    set up extension

---
+++ content.js
@@ -0,0 +1,40 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        "title": 'Open %s in WhatsApp',
+        "contexts": ["selection"],
+        "id": "myContextMenuId"
+    });
+});
+    
+function returnNumber(s) {
+    var number = s.split(" ").join("");
+    if(!isNaN(number)){
+        if(number.length > 10) {
+            return number;
+        }
+        else {
+            var withcode = "91" + number;
+            return withcode;
+        }
+    }
+    else if(number.startsWith("+")){
+        var numbersplit = number.split('+');
+        number = numbersplit[1];
+        if(number.length > 10) {
+            return number;
+        }
+        else {
+            var withcode = "91" + number;
+            return withcode;
+        }
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    var number = info.selectionText;
+    number = returnNumber(number);
+    console.log(number);
+    chrome.tabs.create({  
+        url: "https://api.whatsapp.com/send/?phone=" + number
+    });
+})
